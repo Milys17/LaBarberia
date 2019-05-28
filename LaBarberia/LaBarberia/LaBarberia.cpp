22857185398b2cbbe64123bd93aef5649a2fa280
Milys17 <miriam17_9@outlook.com>
2019-05-28 12:01:40 -0700

    arreglo

--- LaBarberia/LaBarberia/LaBarberia.cpp
+++ LaBarberia/LaBarberia/LaBarberia.cpp
@@ -1,27 +1,103 @@
-// LaBarberia.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include "pch.h"
 #include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include "Cita.h"
+using namespace std;
 
-bool repetir = true;
+int regisActual = 1;
+
+ofstream oFile("citas.txt");
+ifstream iFile("citas.txt");
+
+cita arreglo[200];
+
+void registrarArchivo() {
+	 if (oFile.is_open()) {
+		  for (int x = 0; x < std::size(arreglo); x++) {
+			   std::stringstream  ss;
+			   ss << arreglo[x].getNombre() << " " << arreglo[x].getId() << " " << arreglo[x].getTelefono() << " " << arreglo[x].getBarbero() << " " << arreglo[x].getFecha() << " " << arreglo[x].getHora() << " " << arreglo[x].getTipo() << "\n";
+			   std::string s = ss.str();
+			   oFile << s;
+		  }
+		  oFile.close();
+	 }
+}
+
+void añadirArreglo(cita Cita) {
+	 if (regisActual < 200) {
+		  arreglo[regisActual] = Cita;
+		  registrarArchivo();
+	 }
+	 regisActual++;
+}
+
+void registrar() {
+	 int selec;
+	 std::cout << "\n";
+	 std::cout << "Registrar:\n1) Cita\n2) En fila de espera\n";
+	 std::cin >> selec;
+	 std::cout << "\n";
+	 if (selec == 1) {
+		  string nombre, telefono, barbero, fecha, hora;
+		  std::cout << "Nombre:\n";
+		  std::cin >> nombre;
+		  std::cout << "\n";
+
+		  std::cout << "Telefono:\n";
+		  std::cin >> telefono;
+		  std::cout << "\n";
+
+		  std::cout << "Barbero: (A / B / C / D)\n";
+		  std::cin >> barbero;
+		  std::cout << "\n";
+
+		  std::cout << "Fecha: (DD-MM-AA)\n";
+		  std::cin >> fecha;
+		  std::cout << "\n";
+
+		  std::cout << "Hora: (HH:MM)\n";
+		  std::cin >> hora;
+
+		  cita newCita(std::to_string(regisActual),nombre,telefono,barbero,fecha,hora);
+		  añadirArreglo(newCita);
+	 }
+	 else {
+		  string nombre, fecha;
+		  std::cout << "Nombre:\n";
+		  std::cin >> nombre;
+		  std::cout << "\n";
+
+		  std::cout << "FechaActual";
+		  std::cin >> fecha;
+	
+		  cita newCita(std::to_string(regisActual), nombre, fecha);
+		  añadirArreglo(newCita);
+	 }
+}
+
 
 int main()
 {
+	 bool repetir = true;
 	 while (repetir)
 	 {
-
 		  int selector;
 		  std::cout << "Bienvenido a la Barberia!\n";
-		  std::cout << "¿Que desea hacer?\n1) Registrar\n2) Ver\n3) Modificar\n4) Cancelar\n5) Cerrar";
+		  std::cout << "¿Que desea hacer?\n1) Registrar\n2) Ver\n3) Modificar\n4) Cancelar\n5) Cerrar\n";
 
 		  std::cin >> selector;
 		  switch (selector)
 		  {
 		  case 1:
+			   registrar();
 			   break;
 
 		  case 2:
+			   for (int x = 0; x < regisActual;x++) {
+					cout << arreglo[x].getId() << " " << arreglo[x].getNombre() << " " << arreglo[x].getTelefono() << " " << arreglo[x].getBarbero() << " " << arreglo[x].getFecha() << " " << arreglo[x].getHora() << " " << arreglo[x].getTipo() << "\n";
+			   }
 			   break;
 
 		  case 3:
@@ -35,5 +111,6 @@ int main()
 			   repetir = false;
 			   break;
 		  }
+		  std::cout << "\n";
 	 }
 }
